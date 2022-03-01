6670334b13274608b97b3371d0f212cdc2d507ff
nick8604 <97799306+nick8604@users.noreply.github.com>
2022-03-01 20:17:41 +0530

    done

--- src/components/circle.js
+++ src/components/circle.js
@@ -1,6 +1,16 @@
 // import using require
-
+const Shape=require("./shape");
 // declare class
+class Circle extends Shape{
+    /* constructor(radius=4,color="red"){
+        super("red");
+        this.radius=radius; */
+    //}
+    calculateArea() {
+        return Math.PI*5*5;
+    }
+}
 
 
 // export class using module.exports
+module.exports.Circle=Circle;
--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,14 @@
 // declare class
-
+class Shape {
+   /*  constructor(color="red"){
+        this.color=color;
+    } */
+    color="red";
+    drawshape(){}
+    calculateArea(){}
+    color(){
+        this.color="red";
+    }
+}
 // export class using module.exports
+module.exports.Shape=Shape;
